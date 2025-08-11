1325f0b43e5fc4cca8ddc6a6240c35e6cbe386d0
Andrea <andrea.zerbini8@gmail.com>
2025-08-11 14:07:31 +0200

    Add bean existence assertions to contextLoads test

--- src/test/java/io/curity/example/oidcspringbootmutualtls/OidcSpringBootMutualTlsApplicationTests.java
+++ src/test/java/io/curity/example/oidcspringbootmutualtls/OidcSpringBootMutualTlsApplicationTests.java
@@ -1,13 +1,24 @@
 package io.curity.example.oidcspringbootmutualtls;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OidcSpringBootMutualTlsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private SecurityWebFilterChain securityWebFilterChain;
 
+    @Autowired
+    private TrustStoreConfig trustStoreConfig;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(securityWebFilterChain);
+        assertNotNull(trustStoreConfig);
+    }
 }
